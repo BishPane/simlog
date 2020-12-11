f136be70e7bb5adc6c5b416f11ccb04353ea5909
BishPane <74902845+BishPane@users.noreply.github.com>
2020-12-11 10:55:39 +0545

    Add files via upload

---
+++ AdminCheck.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        
+        if($request->path()=='app/user_login'){
+            return $next($request);
+
+        }
+        if(!Auth::check()){
+            return response()->json([
+                'msg' => 'Cant acess this route',
+                'url' => $request->path()
+            ], 403);
+        }
+
+         $user = Auth::user();
+         $roles = $user->roles;
+         foreach($roles as $role){
+            if($role->name  == "Admin" || $role->name  == "Editor" ){
+                return $next($request);
+            }else{
+                return response()->json([
+                             'msg' => 'Cant acess this route'
+                         ], 403);
+            }
+
+         }
+        //  if($user->roles->name == "User"){
+        //      return response()->json([
+        //          'msg' => 'Cant acess this route'
+        //      ], 403);
+
+        //  }
+
+        return $next($request);
+    }
+}
